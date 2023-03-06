7bc6993253d9a3f05f1c3e424b99d7cca743336e
korsakov <korsakov.ua.dn@gmail.com>
2023-03-06 09:36:24 +0300

    refactoring/divided tests of slice into part: thunks, extraredicer

--- src/modules/board-controller/board-slice/board-slice.test.ts
+++ src/modules/board-controller/board-slice/board-slice.test.ts
@@ -6,7 +6,7 @@ import { startSet } from '../utils';
 
 import { boardReducer, initialState, move, restart } from './index';
 
-describe('Board slice', () => {
+describe('Thunks', () => {
   let store: Store;
 
   beforeAll(() => {
@@ -23,14 +23,10 @@ describe('Board slice', () => {
     });
   });
 
-  it('returns the initial state', () => {
-    expect(boardReducer(undefined, { type: '' })).toEqual(initialState);
-  });
-
   it('should set to state startSet of piece and change loading to false', async () => {
     await store.dispatch(restart());
     expect(store.getState().board.pieces).toEqual(startSet);
-    expect(store.getState().board.loading).toBe(false);
+    expect(store.getState().board.loading).toBeFalsy();
   });
 
   it('should handle MOVE', async () => {
@@ -62,6 +58,35 @@ describe('Board slice', () => {
         color: 'black',
         id: 'khight_black_2',
       },
+    });
+  });
+});
+
+describe('Extrareducer', () => {
+  it('returns the initial state', () => {
+    expect(boardReducer(undefined, { type: '' })).toEqual(initialState);
+  });
+
+  it('should change loading and error', () => {
+    const state = boardReducer(initialState, move.pending);
+    expect(state.loading).toBeTruthy();
+    expect(state.error).toBeNull();
+  });
+
+  it('should change position of rook', () => {
+    const payload = {
+      prev: JSON.stringify(['a', 7]),
+      next: JSON.stringify(['a', 5]),
+    };
+    const state = boardReducer(
+      initialState,
+      move.fulfilled(payload, '', payload)
+    );
+
+    expect(state).toEqual({
+      pieces: { [JSON.stringify(['a', 5])]: undefined },
+      loading: false,
+      error: null,
     });
   });
 });
